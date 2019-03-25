ecf3e62d3cf99add7484d80fe384ed221903fb81
AutoBitacora <autobitacora@gmail.com>
2019-03-24 22:42:08 -0300

    corregido CDN a local

--- index.php
+++ index.php
@@ -3,17 +3,15 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/jquery.mobile-1.4.5.css">
-<script src="js/jquery.js"></script>
+<script src="js/jquery-1.11.3.min.js"></script>
 <script src="js/jquery.mobile-1.4.5.js"></script>
 
-<!-- Include meta tag to ensure proper rendering and touch zooming -->
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
 <!-- Include jQuery Mobile stylesheets -->
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<!-- <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" >-->
+
 
 <!-- Include the jQuery library -->
-<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<!--<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script> -->
 
 <!-- Include the jQuery Mobile library -->
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
